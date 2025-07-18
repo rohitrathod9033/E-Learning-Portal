ff388802e81badd40d2475aefa98f123eedf4b33
Rohit Rathod <rohitrathod60371@gmail.com>
2025-07-19 01:24:16 +0530

    Resolve

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
-import MyEnrollments from './../../pages/student/MyEnrollments';
+
 
 const Navbar = () => {
 
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'} `}>
       
-      <img src={assets} alt="Logo" className='`w-28 lg:w-32 cursor-pointer' />
+      <img src={assets.logo} alt="Logo" className='`w-28 lg:w-32 cursor-pointer' />
       <div className=' hidden md:flex items-center gap-5 text-gray-500'>
         <div>
           <button>Become Educator</button>
