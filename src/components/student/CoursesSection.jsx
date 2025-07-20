af917bf81c3be9a38d56732a17d1fcbbada125dc
Rohit Rathod <rohitrathod60371@gmail.com>
2025-07-20 13:57:02 +0530

    Add All Code

--- src/components/student/CoursesSection.jsx
+++ src/components/student/CoursesSection.jsx
@@ -9,18 +9,16 @@ const CoursesSection = () => {
   return (
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
-      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to <br />business and wellness, our courses are crafted to deliver results.</p>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8'>
-        {allCourses?.slice(0, 4).map((course, index) => (
-          <CourseCard key={index} course={course} />
-        ))}
+      <div className='grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4' >
+        {allCourses. slice(0,4).map((course, index)=> <CourseCard key={index} course={course} />)}
       </div>
-
-      <div className="mt-10 text-center">
-        <Link to={'/course-list'} onClick={() => window.scrollTo(0, 0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded inline-block'>Show all courses</Link>
-      </div>
+      
+      <Link to={'/course-list'} onClick={ ()=> scrollTo(0,0)}
+      className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>Show all courses</Link>
     </div>
+
   )
 }
 
--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -2,22 +2,76 @@
 import { useState, useEffect } from "react";
 import { dummyCourses } from "../assets/assets";
 import { AppContext } from "./AppContext";
+import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContextProvider = ({ children }) => {
   const currency = import.meta.env.VITE_CURRENCY || "₹";
-  const [allCourses, setAllCourses] = useState([]);
 
+  const navigate = useNavigate()
+
+  const [allCourses, setAllCourses] = useState([]);
+  const [isEducator, setIsEducator] = useState([true]); 
+  const [enrolledCourses, setEnrolledCourses] = useState([]); 
+
+  // Fetch All Courses 
   const fetchAllCourses = async () => {
     setAllCourses(dummyCourses);
   };
 
+  // Function to calculate average rating of course
+  const calculateRating = (course)=>{
+    if(course. courseRatings.length === 0){
+      return 0;
+    }
+    let totalRating = 0
+    course. courseRatings. forEach(rating => {
+      totalRating += rating.rating
+    })
+    return totalRating / course.courseRatings.length;
+  }
+
+  // Function which calculte "chapter time"....
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
+    chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+    return humanizeDuration(time * 60 * 1000, {units: ['h', 'm']})
+  }
+
+  // Function which calculte "course time"....
+  const calculateCourseDuration = (course) => {
+    let time = 0; 
+    course.courseContent.map((chapter) => chapter.chapterContent.map(
+      (lecture) => time += lecture.lectureDuration
+    ))
+    return humanizeDuration(time * 60 * 1000, {units: ['h', 'm']})
+  }
+
+  // Function which calculte "Total No. Of Lectures"
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach(chapter => {
+      if(Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length
+      }
+    })
+    return totalLectures
+  }
+
+  // _____________________________________________ 
+
+  // Fetch user enrolled courses...
+  const fetchUserEnrolledCourses = async ()=>{
+    setEnrolledCourses(dummyCourses)
+}
+  
   useEffect(() => {
     fetchAllCourses();
+    fetchUserEnrolledCourses();
   }, []);
 
   const value = {
-    currency,
-    allCourses,
+    currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses, fetchUserEnrolledCourses
   };
 
   return (
