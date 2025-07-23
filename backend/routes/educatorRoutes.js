a5e987eef161d2c35ed335d3c43536f4911a3450
Rohit Rathod <rohitrathod60371@gmail.com>
2025-07-23 21:19:40 +0530

    Push

--- backend/routes/educatorRoutes.js
+++ backend/routes/educatorRoutes.js
@@ -1,19 +1,18 @@
-import express from 'express';
-import { addCourse, updateRoleToEducator } from '../controllers/educatorController.js';
-import upload from '../configs/multer.js';
-import { protectEducator } from '../middlewares/authMiddleware.js';
+import express from "express";
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from "../controllers/educatorController.js";
+import upload from "../configs/multer.js";
+import { protectEducator } from "../middlewares/authMiddleware.js";
 
 const educatorRouter = express.Router();
 
-// Add Educator Role
-educatorRouter.get('/update-role', updateRoleToEducator);
-educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse);
+// Route to update role to Educator
+educatorRouter.get("/update-role", updateRoleToEducator);
+
+// Route to add a new course (POST /api/educator/courses)
+educatorRouter.post("/courses", upload.single("image"), protectEducator, addCourse);
+
+educatorRouter.get("/courses", protectEducator, getEducatorCourses);
+educatorRouter.get("/dashboard", protectEducator, educatorDashboardData);
+educatorRouter.get("/enrolled-students", protectEducator, getEnrolledStudentsData);
 
 export default educatorRouter;
-
-
-
-
-
-
-
