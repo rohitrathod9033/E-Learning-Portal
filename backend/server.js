f8a1e6857fd29fe950f665baab2304221bda7bd3
Rohit Rathod <rohitrathod60371@gmail.com>
2025-07-26 02:51:21 +0530

    Push All

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-// server.js or app.js
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
 import cloudinary from './configs/cloudinary.js';
 
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
 import { clerkMiddleware } from '@clerk/express';
 
 import userRouter from './routes/userRoutes.js';
@@ -24,10 +23,11 @@ app.use(clerkMiddleware());
 
 // Routes
 app.get('/', (req, res) => res.send("API Running"));
-app.post('/clerk', clerkWebhooks);
-app.use('/api/user', userRouter);
-app.use('/api/educator', educatorRouter);
-app.use('/api/course', courseRouter);
+app.post('/clerk', express.json(), clerkWebhooks);
+app.use('/api/user', express.json(), userRouter);
+app.use('/api/educator', express.json(), educatorRouter);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 // Server
 const PORT = process.env.PORT || 5000;
